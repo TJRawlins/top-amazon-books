3bd9fc5b8444f53ac155c19abc4b90db7afb7b96
Electrum <58157207+Electrum-Data@users.noreply.github.com>
2023-12-27 22:18:27 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const books = [
+  {
+    title: "Atomic Habits: An Easy & Proven Way",
+    author: "James Clear",
+    img: "https://images-na.ssl-images-amazon.com/images/W/MEDIAX_792452-T2/images/I/81YkqyaFVEL._AC_UL600_SR600,400_.jpg",
+    id: 1
+  },
+  {
+    title: "Fourth Wing (The Empyrean, 1)",
+    author: "Rebecca Yarros",
+    img: "https://images-na.ssl-images-amazon.com/images/W/MEDIAX_792452-T2/images/I/91n7p-j5aqL._AC_UL600_SR600,400_.jpg",
+    id: 2
+  },
+  {
+    title: "Dinner Tonight: 100 Simple, Healthy Recipes",
+    author: "Alex Snodgrass",
+    img: "https://images-na.ssl-images-amazon.com/images/W/MEDIAX_792452-T2/images/I/811HITbWChL._AC_UL600_SR600,400_.jpg",
+    id: 3
+  },
+];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const BookList = () => {
+  return (
+    <section className="booklist">
+      {books.map((book) => {
+        return <Book {...book} key={book.id} />;
+      })}
+    </section>
+  );
+};
+
+const Book = (props) => {
+  const {author, title, img} = props
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+    </article>
+  );
+};
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+
+root.render(<BookList />);
