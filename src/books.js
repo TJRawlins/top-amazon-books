2b743f2e68ac6ca020fe3b8168d7e1ecffb5e1e5
Electrum <58157207+Electrum-Data@users.noreply.github.com>
2023-12-29 10:01:32 -0700

    refactored components

---
+++ src/books.js
@@ -0,0 +1,20 @@
+export const books = [
+  {
+    title: "Atomic Habits: An Easy & Proven Way",
+    author: "James Clear",
+    img: "https://images-na.ssl-images-amazon.com/images/W/MEDIAX_792452-T2/images/I/81YkqyaFVEL._AC_UL600_SR600,400_.jpg",
+    id: 1,
+  },
+  {
+    title: "Fourth Wing (The Empyrean, 1)",
+    author: "Rebecca Yarros",
+    img: "https://images-na.ssl-images-amazon.com/images/W/MEDIAX_792452-T2/images/I/91n7p-j5aqL._AC_UL600_SR600,400_.jpg",
+    id: 2,
+  },
+  {
+    title: "Dinner Tonight: 100 Simple, Healthy Recipes",
+    author: "Alex Snodgrass",
+    img: "https://images-na.ssl-images-amazon.com/images/W/MEDIAX_792452-T2/images/I/811HITbWChL._AC_UL600_SR600,400_.jpg",
+    id: 3,
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,27 +1,8 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 import "./index.css";
-
-const books = [
-  {
-    title: "Atomic Habits: An Easy & Proven Way",
-    author: "James Clear",
-    img: "https://images-na.ssl-images-amazon.com/images/W/MEDIAX_792452-T2/images/I/81YkqyaFVEL._AC_UL600_SR600,400_.jpg",
-    id: 1
-  },
-  {
-    title: "Fourth Wing (The Empyrean, 1)",
-    author: "Rebecca Yarros",
-    img: "https://images-na.ssl-images-amazon.com/images/W/MEDIAX_792452-T2/images/I/91n7p-j5aqL._AC_UL600_SR600,400_.jpg",
-    id: 2
-  },
-  {
-    title: "Dinner Tonight: 100 Simple, Healthy Recipes",
-    author: "Alex Snodgrass",
-    img: "https://images-na.ssl-images-amazon.com/images/W/MEDIAX_792452-T2/images/I/811HITbWChL._AC_UL600_SR600,400_.jpg",
-    id: 3
-  },
-];
+import { books } from "./books";
+import Book from "./Book";
 
 const BookList = () => {
   return (
@@ -30,17 +11,6 @@ const BookList = () => {
         return <Book {...book} key={book.id} />;
       })}
     </section>
-  );
-};
-
-const Book = (props) => {
-  const {author, title, img} = props
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
-    </article>
   );
 };
 
