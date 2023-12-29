1e17527128f360302ed521360faba5958a726551
Electrum <58157207+Electrum-Data@users.noreply.github.com>
2023-12-29 11:47:17 -0700

    project completed

--- src/Book.js
+++ src/Book.js
@@ -1,11 +1,12 @@
 const Book = (props) => {
-  const { author, title, img } = props;
+  const { author, title, img, number } = props;
   // console.log(props);
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
+      <span className="number">#{number + 1}</span>
     </article>
   );
 };
--- src/books.js
+++ src/books.js
@@ -1,20 +1,24 @@
+import img1 from "./img/atomic-habits.jpg";
+import img2 from "./img/dinner-tonight.jpg";
+import img3 from "./img/fourth-wing.jpg";
+
 export const books = [
   {
     title: "Atomic Habits: An Easy & Proven Way",
     author: "James Clear",
-    img: "https://images-na.ssl-images-amazon.com/images/W/MEDIAX_792452-T2/images/I/81YkqyaFVEL._AC_UL600_SR600,400_.jpg",
+    img: img1,
     id: 1,
   },
   {
     title: "Fourth Wing (The Empyrean, 1)",
-    author: "Rebecca Yarros",
-    img: "https://images-na.ssl-images-amazon.com/images/W/MEDIAX_792452-T2/images/I/91n7p-j5aqL._AC_UL600_SR600,400_.jpg",
+    author: "Alex Snodgrass",
+    img: img2,
     id: 2,
   },
   {
     title: "Dinner Tonight: 100 Simple, Healthy Recipes",
-    author: "Alex Snodgrass",
-    img: "https://images-na.ssl-images-amazon.com/images/W/MEDIAX_792452-T2/images/I/811HITbWChL._AC_UL600_SR600,400_.jpg",
+    author: "Rebecca Yarros",
+    img: img3,
     id: 3,
   },
 ];
